ac4671a67621ad524b4028f2be779b83b237cfb5
wangjitao <wangjitao@rongzi.com>
2016-07-04 15:16:41 +0800

    ////

--- MyApplication01/app/src/main/java/com/example/administrator/myapplication01/activity/SideSlippingActivity.java
+++ MyApplication01/app/src/main/java/com/example/administrator/myapplication01/activity/SideSlippingActivity.java
@@ -2,6 +2,10 @@ package com.example.administrator.myapplication01.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.administrator.myapplication01.R;
 
@@ -9,15 +13,30 @@ import com.example.administrator.myapplication01.R;
  * Created by Administrator on 2016/7/4 0004.
  */
 public class SideSlippingActivity extends Activity {
+
+    private FrameLayout frameLayout;
+    private RelativeLayout left;
+    private ListView left_listview;
+    private RelativeLayout right;
+    private TextView right_textview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.sideslippinglayout);
 
-
+        initView();
 
     }
 
+    private void initView() {
+        frameLayout = (FrameLayout) findViewById(R.id.fragment_layout);
 
+        left = (RelativeLayout) findViewById(R.id.left);
+        left_listview = (ListView) findViewById(R.id.left_listview);
+
+        right = (RelativeLayout) findViewById(R.id.right);
+        right_textview = (TextView) findViewById(R.id.right_textview);
+    }
 }
