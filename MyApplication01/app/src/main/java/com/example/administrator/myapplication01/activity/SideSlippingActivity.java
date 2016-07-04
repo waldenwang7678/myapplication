fc70c67628f5a014befd881029c04a1e95cf595b
wangjitao <wangjitao@rongzi.com>
2016-07-04 18:44:50 +0800

    //

--- MyApplication01/app/src/main/java/com/example/administrator/myapplication01/activity/SideSlippingActivity.java
+++ MyApplication01/app/src/main/java/com/example/administrator/myapplication01/activity/SideSlippingActivity.java
@@ -2,12 +2,17 @@ package com.example.administrator.myapplication01.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.administrator.myapplication01.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2016/7/4 0004.
@@ -19,15 +24,22 @@ public class SideSlippingActivity extends Activity {
     private ListView left_listview;
     private RelativeLayout right;
     private TextView right_textview;
+    private ArrayList<String> datas = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.sideslippinglayout);
-
+        initdata();
         initView();
 
+    }
+
+    private void initdata() {
+        for (int i = 0; i < 20; i++) {
+            datas.add("侧滑数据" + i);
+        }
     }
 
     private void initView() {
@@ -38,5 +50,37 @@ public class SideSlippingActivity extends Activity {
 
         right = (RelativeLayout) findViewById(R.id.right);
         right_textview = (TextView) findViewById(R.id.right_textview);
+        ListAdapter adapter = new ListAdapter();
+        left_listview.setAdapter(adapter);
+
+    }
+
+
+    class ListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return datas.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return datas.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = new TextView(SideSlippingActivity.this);
+            }
+            ((TextView) convertView).setText(datas.get(position));
+
+            return convertView;
+        }
     }
 }
